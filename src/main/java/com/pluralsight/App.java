d4d5625c977fa24e9142798d43c195b0eeb93a42
iam-nadiii <naodd43@gmail.com>
2026-04-21 23:01:49 -0500

    completed both my fileReader and my fileWriter methods

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,18 +1,38 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.io.IOException;
+import java.util.Scanner;
+
+import static com.pluralsight.Employee.employeesArray;
 
 public class App {
 
     static int index = 0;
-    public static void main(String[] args) {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+
+        readEmployeeInfoFile();
+
+        printEmployeeInfo(employeesArray);
+
+
+        createEmployeeInfoFile();
+
+
+    }
+
+
+    private static void readEmployeeInfoFile() {
+        System.out.print("Enter the name of the file employee file to process: ");
+        String fileNameToRead = scanner.next();
+
+
         String input;
-        Employee[] employeesArray = new Employee[8];
+
 
         try{
-            FileReader fileReader = new FileReader("employees.csv");
+            FileReader fileReader = new FileReader(fileNameToRead);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             boolean firstLine = true;
             while( (input = bufferedReader.readLine()) != null) {
@@ -31,10 +51,48 @@ public class App {
         } catch (IOException e){
             e.printStackTrace();
         }
-
-        printEmployeeInfo(employeesArray);
-
     }
+
+    private static void createEmployeeInfoFile() {
+        System.out.print("Enter the name of the payroll file to create: ");
+        String fileNameToWrite = scanner.next();
+
+
+        try{
+            FileWriter fileWriter = new FileWriter(fileNameToWrite);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            if(fileNameToWrite.endsWith(".txt")) {
+
+                bufferedWriter.write("id|name|gross pay\n");
+
+                for (Employee employee : employeesArray) {
+                    bufferedWriter.write(employee.getEmployeeId() + "|" + employee.getName() + "|$" +
+                            employee.getGrossPay() + "\n");
+                }
+            } else if(fileNameToWrite.endsWith(".json")){
+                bufferedWriter.write("[\n");
+
+                for (Employee employee : employeesArray) {
+                    bufferedWriter.write("{\"id\": " + employee.getEmployeeId() + ", \"name\": " +
+                            employee.getName() + ", \"grossPay\": $" + employee.getGrossPay() + "}\n");
+                }
+                bufferedWriter.write("]");
+
+            } else {
+                System.out.println("Error: File name has to either end with \"txt\" or \"json\". Please try again");
+                createEmployeeInfoFile();
+            }
+
+
+            bufferedWriter.close();
+
+        } catch (IOException e){
+            System.out.println("ERROR: An unexpected error occurred");
+            e.printStackTrace();
+        }
+    }
+
 
     private static void printEmployeeInfo(Employee[] employeesArray) {
         for(Employee employee: employeesArray){
@@ -56,3 +114,8 @@ public class App {
         index++;
     }
 }
+
+
+
+
+//        ]
--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,7 +1,7 @@
 package com.pluralsight;
 
 public class Employee {
-    public Employee[] employeeList = new Employee[8];
+    public static Employee[] employeesArray = new Employee[8];
     private int employeeId;
     private String name;
     private double hoursWorked;
@@ -53,7 +53,7 @@ public class Employee {
 
     public double getGrossPay(){
         if (hoursWorked > 40){
-            return 40 * hoursWorked + (hoursWorked - 40) * payRate * 0.5;
+            return hoursWorked * payRate + (hoursWorked - 40) * payRate * 0.5;
         }
         return hoursWorked * payRate;
     }
@@ -64,30 +64,3 @@ public class Employee {
 }
 
 
-
-//Step 1
-//Create an Employee class as described below:
-//Private data members (attributes):
-//        - employeeId
-//- name
-//- hoursWorked
-//- payRate
-//Methods:
-//        - parameterized constructor
-//- getEmployeeId() and getters/setters for other attributes
-//- getGrossPay() that calculates and returns that employee's gross pay based
-//on their hours worked and pay rate
-
-//Step 2
-//To make the main program work, you will need to:
-//        1. Load the file using a FileReader object
-//2. Read each line of text
-//3. Split it into individual fields using the | character as the delimiter
-//4. Copy the values from the tokens array into variables that match the data and then
-//use them to create a new Employee object
-//5. Display the employee using a printf and by calling the employee's
-//getEmployeeId(), getName(), and getGrossPay() methods
-//6. Repeat for each line in the input file
-//Step 3
-//        1. Ensure all your changes are committed and pushed to GitHub
-//2. Send your repository URL to your Instructor
